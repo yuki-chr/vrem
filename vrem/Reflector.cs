5273088057953b2a010daed9b338ad9a23fb23ed
lorenzo-barzanti <43414325+yuki-chr@users.noreply.github.com>
2025-08-29 13:15:01 +0200

    testing an alternative reflector
    need to figure out why rotation is not rotating

--- vrem/Reflector.cs
+++ vrem/Reflector.cs
@@ -9,16 +9,25 @@ namespace vrem
     internal class Reflector(string key) : ICharPipeline
     {
         private const int KeySize = 256;
+        private bool selfcrypt = false;
 
         public byte Process(byte b)
         {
-            return (byte)((b+(KeySize/2))%KeySize); //TODO implement mirrored transformation
-            //reflector should rotate every single call on top of every time its rotate method is called
+            if (selfcrypt)
+            {
+                selfcrypt = false;
+                return b;
+            }
+            else
+            {
+                return (byte)((b + (KeySize / 2)) % KeySize); //TODO implement mirrored transformation
+            }
         }
 
         public void Rotate()
         {
-            //TODO
+            //testing alternative method to deal with statistical analysis
+            selfcrypt = true;
         }
     }
 }
